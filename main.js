82218844369b2c42f8b6323220cc7a1015b45b09
leeCodingStudio <lehj2308@gmail.com>
2023-08-09 13:28:47 +0900

    클라이언트 수정

--- main.js
+++ main.js
@@ -160,11 +160,16 @@ async function server_chat(context) {
         .then(datas => {
             datas.forEach(data => {
                 //  대답이 면접관 텍스트로 들어가는 과정
-                if(data == '없음'){
-                    data = result_box_list.children[count_idx].cloneNode(true).textContent
+                if (data == 'Nan') {
+                    ggory = 0
+                    chat_text.value = ''
+                    count_idx++
+                    chat_list.innerHTML += `<li class="interviewer_li"><span class="icon"></span><p class="interviewer">${result_box_list.children[count_idx].cloneNode(true).textContent}</p></li>`;
+                    chat_list.lastElementChild.scrollIntoView({ behavior: "smooth" })
+                } else {
+                    chat_list.innerHTML += `<li class="interviewer_li"><span class="icon"></span><p class="interviewer">${data}</p></li>`;
+                    chat_list.lastElementChild.scrollIntoView({ behavior: "smooth" })
                 }
-                chat_list.innerHTML += `<li class="interviewer_li"><span class="icon"></span><p class="interviewer">${data}</p></li>`;
-                chat_list.lastElementChild.scrollIntoView({ behavior: "smooth" })
             });
         })
         .then(() => {
